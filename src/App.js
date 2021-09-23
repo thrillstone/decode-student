6e3e1b5692690cb6d2a9c9666d15a77ca64b72cd
Kevin Lidstone <kevin.lidstone@solace.com>
2021-09-23 18:42:31 -0400

    Add navigation structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Login from './components/login/Login';
+import Landing from './components/landing/Landing';
+import ClassroomSelector from './components/classroom/ClassroomSelector';
+import Classroom from './components/classroom/Classroom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+  const [nav, setNav] = useState(null);
+  const [classroom, setClassroom] = useState(null);
+
+  function handleLogin(name) {
+    setUser(name);
+  }
+
+  function handleNavClick(nav) {
+    setNav(nav);
+  }
+
+  function handleClassroomSelected(nav) {
+    setClassroom(nav);
+  }
+
+  if (!user) {
+    return (
+      <Login onLogin={handleLogin}></Login>
+    );
+  } else if (!nav) {
+    return (
+      <div>
+        <h1>Welcome {user} </h1>
+        <Landing onNavClick={handleNavClick}></Landing>
+      </div>
+    )
+  } else {
+    if (!classroom) {
+      return (
+        <ClassroomSelector onClasroomSelected={handleClassroomSelected}></ClassroomSelector>
+      )
+    } else {
+      return (
+        <Classroom></Classroom>
+      )
+    }
+  }
 }
 
 export default App;
