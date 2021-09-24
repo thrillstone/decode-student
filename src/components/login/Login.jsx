ee708af57062e8693c0da0b5b52bcdf87fb7f729
GrantvanB <gvanbo@gmail.com>
2021-09-24 11:50:32 -0600

    installed uuid from npm and can createUser

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,28 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { MessagingServiceContext } from "../../App";
+
 
 function Login({ onLogin }) {
   const [name, setName] = useState("");
+  const messagingService = useContext(MessagingServiceContext);
+  
+  
+  const handleClick = () => {  
+    onLogin(name)
+    createUser(name)    
+  }
+
+  const createUser = (name) => {
+    const userId = uuidv4()
+    const user = {
+      "userId": userId,
+      "name": name,
+      "message": ''
+    }
+    console.log(user)
+  }
+
   return (
     <div className="App">
      <div>
@@ -11,7 +32,7 @@ function Login({ onLogin }) {
        <input value={ name } type="text" onChange={e => setName(e.target.value)}></input>
      </div>
      <div>
-       <button onClick={() => onLogin(name)}>Login</button>
+       <button onClick={handleClick}>Login</button>
      </div>
     </div>
   );
