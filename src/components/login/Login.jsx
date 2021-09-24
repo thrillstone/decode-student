72f0d4912182d035103f2d80eb87ed0f84e4f74d
GrantvanB <gvanbo@gmail.com>
2021-09-24 13:08:40 -0600

    userId is available to student app

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,39 +1,27 @@
-import { useState, useContext, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import { MessagingServiceContext } from "../../App";
-
+import { useState } from "react";
 
 function Login({ onLogin }) {
-  const [name, setName] = useState("");
-  const messagingService = useContext(MessagingServiceContext);
-  
-  
-  const handleClick = () => {  
-    onLogin(name)
-    createUser(name)    
-  }
+  const [name, setName] = useState('');
 
-  const createUser = (name) => {
-    const userId = uuidv4()
-    const user = {
-      "userId": userId,
-      "name": name,
-      "message": ''
-    }
-    console.log(user)
-  }
+  const handleClick = () => {
+    onLogin(name);
+  };
 
   return (
     <div className="App">
-     <div>
-       <label>Name</label>
-     </div>
-     <div>
-       <input value={ name } type="text" onChange={e => setName(e.target.value)}></input>
-     </div>
-     <div>
-       <button onClick={handleClick}>Login</button>
-     </div>
+      <div>
+        <label>Name</label>
+      </div>
+      <div>
+        <input
+          value={name}
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <button onClick={handleClick}>Login</button>
+      </div>
     </div>
   );
 }
