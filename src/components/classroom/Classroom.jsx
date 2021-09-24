5da91e0eeff6a1e597c5af22fcc8968c3b110813
udit-329 <33633390+udit-329@users.noreply.github.com>
2021-09-24 13:07:17 -0400

    receive messages (#5)

--- src/components/classroom/Classroom.jsx
+++ src/components/classroom/Classroom.jsx
@@ -2,12 +2,41 @@ import Storybook from '../storybook/storybook';
 import {exampleSlides} from '../storybook/exampleSlides';
 import Chat from '../chat/chatContainer.component';
 import FullScreen from '../styles/FullScreen';
+import { useContext, useEffect, useState } from 'react';
+import { MessagingServiceContext } from '../../App';
+import Poll from '../poll/Poll';
 
+const data = {
+  "question":"hi",
+  "pollId":"011",
+  "choices":[
+    {
+      "choiceId":"001",
+      "description":"hindn"
+    },
+    {
+      "choiceId":"0012",
+      "description":"hiffn"
+    }
+  ]
+}
+
+const studentId = "abjcs"
 function Classroom() {
+  
+  const [pollId, setPollId] = useState('');
+  const messagingService = useContext(MessagingServiceContext);
+  
+  useEffect(() => {
+    messagingService.subscribeToTopic("workbook/poll", (pollEvent) => {setPollId(pollEvent.pollId);});
+  }, [messagingService, pollId]);
+  
+
   return (
     <FullScreen>
       <Storybook slides={exampleSlides}></Storybook>
       <Chat />
+      {pollId !== '' && <Poll data={data} studentId={studentId}></Poll>}
     </FullScreen>
   );
 }
