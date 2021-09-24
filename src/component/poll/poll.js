d9002d816b86fd2353d5fb8fa6027cd6681a44a7
Udit Kapoor <ukapoor.29@gmail.com>
2021-09-24 10:31:12 -0400

    Squashed commit of the following:
    commit 18268dacb0e312a3d2ec8bb0c29e6e849b59275c
    Author: Udit Kapoor <ukapoor.29@gmail.com>
    Date:   Thu Sep 23 21:02:27 2021 -0400
    
        poll app

---
+++ src/component/poll/poll.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function onSubmit(pollId, choiceId, studentId) {
+    const reply = {
+        "studentId": studentId,
+        "pollId": pollId,
+        "choiceId": choiceId
+    }
+    console.log(reply)
+}
+
+function Poll(data, studentId) {
+    const [selectedChoice, setChoice] = useState(data.choices[0].choiceId);
+
+    function radioButtons(data) {
+        let choices = [];
+        for (let i = 0; i < data.choices.length; i++) {
+            choices.push(<div className="radio-btn">
+                <input type="radio" value={data.choices[i].choiceId} onChange={() => setChoice(data.choices[i].choiceId)} checked={data.choices[i].choiceId === selectedChoice}/>
+                <label>{data.choices[i].description}</label></div>
+                );
+        }
+        return choices
+    }
+
+    return(
+        <div>
+            <h3>Poll</h3>
+            <h4>{data.question}</h4>
+
+            <form>
+                {radioButtons(data)}
+                <button type="submit" onClick={() => {onSubmit(data.pollId, selectedChoice, studentId)}}>Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Poll;
