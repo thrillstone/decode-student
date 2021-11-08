536150463f18c1005fe7c0c9ed99208215e618ef
Kevin Lidstone <kevin.lidstone@solace.com>
2021-11-08 12:56:03 -0500

    Update VMR

--- src/vmrConfig.js
+++ src/vmrConfig.js
@@ -1,7 +1,7 @@
 const connect = {
-  url: "wss://mrb0bu9q4qk2s.messaging.solace.cloud:443",
-  vpnName: "decode",
+  url: "wss://mroyppj81pv8b.messaging.solace.cloud:443",
+  vpnName: "kevins-ivmr",
   userName: "solace-cloud-client",
-  password: "4n117nhd7rsfmo9mo8r4qd632f",
+  password: "a54s499q2b7jmce1thipn6r974",
 };
 export default connect;
